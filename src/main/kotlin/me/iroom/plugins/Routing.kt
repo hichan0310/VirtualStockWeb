735c30a2ad74a36b88144dc1d342b322ae91a1c5
hichan0310 <maxmath0310@gmail.com>
2022-12-22 09:05:50 +0900

    Initial commit

---
+++ src/main/kotlin/me/iroom/plugins/Routing.kt
@@ -0,0 +1,29 @@
+package me.iroom.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.html.*
+import io.ktor.server.http.content.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import io.ktor.server.routing.get
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.head
+import kotlinx.html.title
+import java.io.File
+
+fun Application.configureRouting() {
+    routing {
+        static("/static") {
+            resources("static")
+        }
+        get("/") {
+            call.respondText("asdf")
+        }
+        get("/index.html") {
+            call.respondFile(File(".\\html\\index.html"))
+        }
+    }
+}
